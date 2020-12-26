a22070333048d75c624bcedf90c565fb828116b7
MargaritaPustovalova <71708472+MargaritaPustovalova@users.noreply.github.com>
2020-12-26 23:40:06 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        long deposit = 2000000000;
+        long income = 500000000;
+        long total = deposit + income;
+        System.out.println(total);
+    }
+}
