c9f26ef870fc4f81bd6d5c8826b2fe76b41989b6
MargaritaPustovalova <71708472+MargaritaPustovalova@users.noreply.github.com>
2020-12-31 17:10:48 +0300

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        long price = 2000000000;
+        long count = 500000000;
+        long total = price * count;
+        System.out.println(total);
+    }
+}
