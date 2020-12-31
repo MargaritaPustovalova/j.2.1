bcce5ac894bc586d417fb30faa66524dc41192d9
MargaritaPustovalova <71708472+MargaritaPustovalova@users.noreply.github.com>
2020-12-31 17:09:09 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int price = 2000000000;
-        int count = 5000000;
+        int count = 500000000;
         int total = price * count;
         System.out.println(total);
     }
